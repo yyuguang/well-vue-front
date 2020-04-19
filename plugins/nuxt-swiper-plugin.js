cd99f5494d426bac3ef707eefcb8e602402fdffa
yyuguang <790335255@qq.com>
2020-04-19 16:37:57 +0800

    Initial commit

---
+++ plugins/nuxt-swiper-plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @author 冷暖自知
+ * @date 2020/4/19 9:03
+ * @version 1.0
+ * @Description
+ */
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr'
+
+Vue.use(VueAwesomeSwiper)
